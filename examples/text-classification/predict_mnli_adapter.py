098269f11e434d090eec08f9a5bc4c8dca8251af
Nelson Liu <nfliu@nelsonliu.me>
2021-04-14 12:36:06 -0700

    Remove unused function

--- examples/text-classification/predict_mnli_adapter.py
+++ examples/text-classification/predict_mnli_adapter.py
@@ -7,16 +7,6 @@ from tqdm import tqdm
 from transformers import AutoModelWithHeads, AutoTokenizer, TextClassificationPipeline
 
 
-def mean_pooling(model_output, attention_mask):
-    token_embeddings = model_output[
-        0
-    ]  # First element of model_output contains all token embeddings
-    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
-    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
-    return sum_embeddings / sum_mask
-
-
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
