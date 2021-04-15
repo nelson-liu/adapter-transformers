faa0b56c9eca5b2e8b4eb37077663665ec5e47d2
Nelson Liu <nfliu@nelsonliu.me>
2021-04-14 18:34:45 -0700

    Override __call__ in Pipeline

--- examples/text-classification/truncating_pipeline.py
+++ examples/text-classification/truncating_pipeline.py
@@ -1,4 +1,5 @@
 from transformers import Pipeline
+import numpy as np
 
 class TruncatingPipeline(Pipeline):
     """
@@ -52,3 +53,34 @@ class TruncatingTextClassificationPipeline(TruncatingPipeline):
     the up-to-date list of available models on `huggingface.co/models
     <https://huggingface.co/models?filter=text-classification>`__.
     """
+    def __call__(self, *args, **kwargs):
+        """
+        Classify the text(s) given as inputs.
+
+        Args:
+            args (:obj:`str` or :obj:`List[str]`):
+                One or several texts (or one list of prompts) to classify.
+
+        Return:
+            A list or a list of list of :obj:`dict`: Each result comes as list of dictionaries with the following keys:
+
+            - **label** (:obj:`str`) -- The label predicted.
+            - **score** (:obj:`float`) -- The corresponding probability.
+
+            If ``self.return_all_scores=True``, one such dictionary is returned per label.
+        """
+        outputs = super().__call__(*args, **kwargs)
+
+        if self.model.config.num_labels == 1:
+            scores = 1.0 / (1.0 + np.exp(-outputs))
+        else:
+            scores = np.exp(outputs) / np.exp(outputs).sum(-1, keepdims=True)
+        if self.return_all_scores:
+            return [
+                [{"label": self.model.config.id2label[i], "score": score.item()} for i, score in enumerate(item)]
+                for item in scores
+            ]
+        else:
+            return [
+                {"label": self.model.config.id2label[item.argmax()], "score": item.max().item()} for item in scores
+            ]
